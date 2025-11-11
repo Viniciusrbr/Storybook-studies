e0cd0f90032b19f9acd3796696a2500623c34dfe
viniciusrbr <viniciusgoes159@gmail.com>
2025-11-11 18:59:22 -0300

    setup storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,31 @@
+import type { Preview, ReactRenderer } from '@storybook/react-vite'
+import { withThemeByClassName } from '@storybook/addon-themes';
+import '../src/index.css' // global css import
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
+  },
+  decorators: [
+    withThemeByClassName<ReactRenderer>({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+};
+
+export default preview;
---
+++ tailwind.config.ts
@@ -0,0 +1,5 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './.storybook/**/*.{js,ts,jsx,tsx}'],
+} satisfies Config
