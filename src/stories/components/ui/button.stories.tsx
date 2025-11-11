ed6dddb77a3786fb62f261cd86f96c2e3069dd7d
viniciusrbr <viniciusgoes159@gmail.com>
2025-11-11 19:41:55 -0300

    title, layout e render

---
+++ src/stories/components/ui/button.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "@/components/ui/button";
+
+const meta = {
+  title: "Components/Button",
+  component: Button,
+  args: {
+    children: "Button",
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: "Primario",
+};
+
+export const Destructive: Story = {
+  args:{
+    children: 'Destructive',
+    variant: 'destructive',
+  },
+  render: (args) => (
+    <Button {...args}>
+      ⚠️ Alerta
+    </Button>
+)
+};
+
+export const Outline: Story = {
+  args:{
+    children: 'Outline',
+    variant: 'outline',
+  }
+};
+
+export const Secondary: Story = {
+  args:{
+    children: 'Secondary',
+    variant: 'secondary',
+  },
+};
+
+export const Ghost: Story = {
+  args:{
+    children: 'Ghost',
+    variant: 'ghost',
+  }
+};
+
+export const Link: Story = {
+  args:{
+    children: 'Link',
+    variant: 'link',
+  }
+};
